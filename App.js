e46a863740d6a9e66fc655330997a540b0495a06
Manas <manaskhantal@gmail.com>
2023-06-15 19:54:33 +0530

    First

--- App.js
+++ App.js
@@ -1,54 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, Button, View } from 'react-native';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { View, Image, Text, StyleSheet, SafeAreaView, TextInput, TouchableOpacity, ActivityIndicator } from 'react-native';
 import Voice from '@react-native-voice/voice';
 
 export default function App() {
-  let [started, setStarted] = useState(false);
-  let [results, setResults] = useState([]);
+  const [result, setResult] = useState('')
+  const [isLoading, setLoading] = useState(false)
 
   useEffect(() => {
-    Voice.onSpeechError = onSpeechError;
-    Voice.onSpeechResults = onSpeechResults;
+    Voice.onSpeechStart = onSpeechStartHandler;
+    Voice.onSpeechEnd = onSpeechEndHandler;
+    Voice.onSpeechResults = onSpeechResultsHandler;
 
     return () => {
       Voice.destroy().then(Voice.removeAllListeners);
     }
-  }, []);
+  }, [])
 
-  const startSpeechToText = async () => {
-    await Voice.start("hi-IN");
-    setStarted(true);
-  };
+  const onSpeechStartHandler = (e) => {
+    console.log("start handler==>>>", e)
+  }
+  const onSpeechEndHandler = (e) => {
+    setLoading(false)
+    console.log("stop handler", e)
+  }
 
-  const stopSpeechToText = async () => {
-    await Voice.stop();
-    setStarted(false);
-  };
+  const onSpeechResultsHandler = (e) => {
+    let text = e.value[0]
+    setResult(text)
+    console.log("speech result handler", e)
+  }
 
-  const onSpeechResults = (result) => {
-    setResults(result.value);
-  };
+  const startRecording = async () => {
+    setLoading(true)
+    try {
+      await Voice.start('hi-IN')
+    } catch (error) {
+      console.log("error raised", error)
+    }
+  }
 
-  const onSpeechError = (error) => {
-    console.log(error);
-  };
+  const stopRecording = async () => {
+    try {
+      await Voice.stop()
+    } catch (error) {
+      console.log("error raised", error)
+    }
+  }
+
 
   return (
     <View style={styles.container}>
-      {!started ? <Button title='Start Speech to Text' onPress={startSpeechToText} /> : undefined}
-      {started ? <Button title='Stop Speech to Text' onPress={stopSpeechToText} /> : undefined}
-      {results.map((result, index) => <Text key={index}>{result}</Text>)}
-      <StatusBar style="auto" />
+      <SafeAreaView>
+        <Text style={styles.headingText}>Agro Grow</Text>
+        <View style={styles.textInputStyle}>
+          <TextInput
+            value={result}
+            placeholder="your text"
+            style={{ flex: 1 }}
+            onChangeText={text => setResult(text)}
+          />
+          {isLoading ? <ActivityIndicator size="large" color="red" />
+
+            :
+            
+            <TouchableOpacity
+              onPress={startRecording}
+            >
+              <Image
+                source={{ uri: 'https://raw.githubusercontent.com/AboutReact/sampleresource/master/microphone.png' }}
+                style={{ width: 25, height: 25 }}
+              />
+            </TouchableOpacity>}
+        </View>
+
+        <TouchableOpacity
+          style={{
+            alignSelf: 'center',
+            marginTop: 24,
+            backgroundColor: 'red',
+            padding: 8,
+            borderRadius: 4
+          }}
+          onPress={stopRecording}
+        >
+          <Text style={{ color: 'white', fontWeight: 'bold' }}>Stop</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 24
   },
+  headingText: {
+    alignSelf: 'center',
+    marginVertical: 26,
+    fontWeight: 'bold',
+    fontSize: 26
+  },
+  textInputStyle: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    height: 48,
+    borderRadius: 20,
+    paddingHorizontal: 16,
+    shadowOffset: { width: 0, height: 1 },
+    shadowRadius: 2,
+    elevation: 2,
+    shadowOpacity: 0.4
+  }
 });
